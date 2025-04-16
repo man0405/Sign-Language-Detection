4be820bb3d92a900240856443a4aeb29c4e9aabd
man0405 <vanman160504@gmail.com>
2025-04-17 00:59:47 +0700

    update

--- module/mediapipe_utils.py
+++ module/mediapipe_utils.py
@@ -40,14 +40,14 @@ def draw_landmarks(image, results):
     Returns:
         None (modifies image in-place)
     """
-    # Draw face landmarks
-    mp_drawing.draw_landmarks(
-        image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
-        mp_drawing.DrawingSpec(color=(80, 110, 10),
-                               thickness=1, circle_radius=1),
-        mp_drawing.DrawingSpec(color=(80, 256, 121),
-                               thickness=1, circle_radius=1)
-    )
+    # # Draw face landmarks
+    # mp_drawing.draw_landmarks(
+    #     image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+    #     mp_drawing.DrawingSpec(color=(80, 110, 10),
+    #                            thickness=1, circle_radius=1),
+    #     mp_drawing.DrawingSpec(color=(80, 256, 121),
+    #                            thickness=1, circle_radius=1)
+    # )
 
     # Draw pose landmarks
     mp_drawing.draw_landmarks(
